9eb125b9e8bd51aa3e0a0f2a774138316e0e8950
MemphisAton <MemphisAton@gmail.com>
2023-10-23 18:00:58 +0300

    Добавление authenticate и login

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'debug_toolbar',
     'women.apps.WomenConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- sitewomen/urls.py
+++ sitewomen/urls.py
@@ -25,7 +25,8 @@ from women.views import page_not_found
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls')),
-    path('__debug__/', include('debug_toolbar.urls'))
+    path('__debug__/', include('debug_toolbar.urls')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
